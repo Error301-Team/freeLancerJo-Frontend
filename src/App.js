befb4a4eae950493adc8ae63a1e5bb62a2531759
DuhaAbuGhazleh <duhamahmoud0@gmail.com>
2021-09-26 15:03:20 +0300

    form and card

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import React, { Component } from 'react'
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
-
+import CardForFreelancer from './components/CardForFreelancer';
+import FreeLancerForm from './components/FreeLancerForm';
 import axios from 'axios'
 // import { withAuth0 } from '@auth0/auth0-react'
 
@@ -16,6 +17,8 @@ class App extends Component {
         <Header />
         <Main />
         <Footer />
+        <CardForFreelancer/>
+        <FreeLancerForm />
       </>
     )
   }
