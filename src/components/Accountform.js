395156e5ab9aa19dbdff2acd1b7b062b035df8c1
WasemIdres <thewasem7915@gmail.com>
2021-09-27 20:44:28 +0300

    added user

--- src/components/Accountform.js
+++ src/components/Accountform.js
@@ -1,151 +1,164 @@
 import React, { Component } from 'react'
 import { Form, Col, Button } from 'react-bootstrap'
+import axios from 'axios';
 import './Accountform.css'
 class Accountform extends Component {
-    
     constructor(props) {
         super(props);
         this.state = {
             firstname: '',
-            surname:'',
-            name:'',
+            surname: '',
+            name: '',
             phononumber: '',
             email: '',
-            education: '',
+            School: '',
             jobdescription: '',
-            skills: [],
-            languages: [],
+            skills: "",
+            lauguages: "",
             price: '',
             location: '',
-            country: '',
-            region: '',
-            experience:'',
-            img:''
+            AreaOfStudys: '',
+            Degree: '',
+            experience: '',
+            img: '',
+            From: '',
+            To: '',
+            Education:{School:"", AreaOfStudys:"",Degree:"",To:"",From:""},
+            usersList:[],
         }
     }
-
     submitHandle = async (e) => {
         e.preventDefault();
-        console.log(this.state.firstname);
-        console.log(this.state.surname);
+        let AreaOfStudys=this.state.To;
+        let Degree=this.state.From;
+        let To=this.state.AreaOfStudys;
+        let From=this.state.AreaOfStudys;
+        let School= this.state.School
         let data = {
             name: `${this.state.firstname} ${this.state.surname}`,
             phononumber: this.state.phononumber,
             email: this.state.email,
-            education: this.state.education,
             jobdescription: this.state.jobdescription,
             skills: this.state.skills.split(','),
-            languages: this.state.languages.split(','),
+            lauguages: this.state.lauguages.split(','),
             price: this.state.price,
             location: this.state.location,
-            country: this.state.country,
-            region: this.state.region,
-            experience:this.state.experience
+            experience: this.state.experience,
+            Education:{AreaOfStudys:AreaOfStudys,Degree:Degree,From:From,To:To,School:School},
+            madeBy:"Wasem",
+            accoutType:"freelacer",
+            auth0:"done",   
         }
+        console.log(data);
+        let newUsersList=await axios.post(`${process.env.REACT_APP_BACKEND_URL}/createUser`, data);
+        console.log(newUsersList);
 
-        // console.log(process.env.REACT_APP_BACKEND_API_KEY);
-        // let job = axios.post(`${process.env.REACT_APP_BACKEND_API_KEY}createJob`, data);
+
     }
 
-    handleChangename=(e)=>{
-        let firstname= e.target.value;
+    handleChangesurname = (e) => {
+        let surname = e.target.value;
         this.setState({
-            firstname:firstname
+            surname: surname
         })
     }
 
-    handleChangesurname=(e)=>{
-        let surname= e.target.value;
+    handleChangephononumber = (e) => {
+        let phononumber = e.target.value;
         this.setState({
-            surname:surname
+            phononumber: phononumber
         })
     }
 
-    handleChangephononumber=(e)=>{
-        let phononumber= e.target.value;
+    handleChangeemail = (e) => {
+        let email = e.target.value;
         this.setState({
-            phononumber:phononumber
-        })
-    }
-    
-    handleChangeemail=(e)=>{
-        let email= e.target.value;
-        this.setState({
-            email:email
+            email: email
         })
     }
 
-    handleChangeeducation=(e)=>{
-        let education= e.target.value;
+    handleChangeSchool = (e) => {
+        let School = e.target.value;
         this.setState({
-            education:education
+            School: School
         })
     }
 
-    handleChangejobdescription=(e)=>{
-        let jobdescription= e.target.value;
+    handleChangejobdescription = (e) => {
+        let jobdescription = e.target.value;
         this.setState({
-            jobdescription:jobdescription
+            jobdescription: jobdescription
         })
     }
 
-    handleChangeskills=(e)=>{
-        let skills= e.target.value;
+    handleChangeskills = (e) => {
+        let skills = e.target.value;
         this.setState({
-            skills:skills
+            skills: skills
         })
     }
 
-    handleChangelanguages=(e)=>{
-        let languages= e.target.value;
+    handleChangelauguages = (e) => {
+        let lauguages = e.target.value;
         this.setState({
-            languages:languages
+            lauguages: lauguages
         })
     }
 
-    handleChangeprice=(e)=>{
-        let price= e.target.value;
+    handleChangeprice = (e) => {
+        let price = e.target.value;
         this.setState({
-            price:price
+            price: price
 
         })
     }
 
-    handleChangelocation=(e)=>{
-        let location= e.target.value;
+    handleChangelocation = (e) => {
+        let location = e.target.value;
         this.setState({
-            location:location
+            location: location
         })
     }
 
-    handleChangecountry=(e)=>{
-        let country= e.target.value;
+    handleChangeAreaOfStudys = (e) => {
+        let AreaOfStudys = e.target.value;
         this.setState({
-            country:country
+            AreaOfStudys: AreaOfStudys
         })
     }
 
-    handleChangeregion=(e)=>{
-        let region= e.target.value;
+    handleChangeDegree = (e) => {
+        let Degree = e.target.value;
         this.setState({
-            region:region
+            Degree: Degree
         })
     }
 
-    handleChangeexperience=(e)=>{
-        let experience= e.target.value;
+    handleChangeexperience = (e) => {
+        let experience = e.target.value;
         this.setState({
-            experience:experience
+            experience: experience
         })
     }
 
-    handleChangeimg=(e)=>{
-        let img= e.target.value;
+    handleChangeimg = (e) => {
+        let img = e.target.value;
         this.setState({
-            img:img
+            img: img
         })
     }
-
+    handleChangeFrom = (e) => {
+        let From = e.target.value;
+        this.setState({
+            From: From
+        })
+    }
+    handleChangeTo = (e) => {
+        let To = e.target.value;
+        this.setState({
+            To: To
+        })
+    }
     render() {
         return (
             <div class="container rounded bg-white mt-5 mb-5">
@@ -153,7 +166,7 @@ class Accountform extends Component {
                     <div class="row">
                         <div class="col-md-3 border-right">
                             <div class="d-flex flex-column align-items-center text-center p-3 py-5"><img class="rounded-circle mt-5" width="150px" src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg" /><span class="font-weight-bold">Edogaru</span><span class="text-black-50">edogaru@mail.com.my</span><span> </span></div>
-                            <div id='centerit' class="col-md-6"><label class="labels">Image Link:</label><input onChange={this.handleChangeregion} type="text" class="form-control" placeholder="Image Link" /></div>
+                            <div id='centerit' class="col-md-6"><label class="labels">Image Link:</label><input onChange={this.handleChangeDegree} type="text" class="form-control" placeholder="Image Link" /></div>
                         </div>
                         <div class="col-md-5 border-right">
                             <div class="p-3 py-5">
@@ -167,12 +180,28 @@ class Accountform extends Component {
                                 <div class="row mt-3">
                                     <div class="col-md-12"><label class="labels">Phone</label><input onChange={this.handleChangephononumber} type="text" class="form-control" placeholder="enter phone number" /></div>
                                     <div class="col-md-12"><label class="labels">Email ID</label><input onChange={this.handleChangeemail} type="text" class="form-control" placeholder="example@email.com" /></div>
-                                    <div class="col-md-12"><label class="labels">Location</label><input onChange={this.handleChangelocation} type="text" class="form-control" placeholder="123 Main Street, That house." /></div>
-                                    <div class="col-md-12"><label class="labels">Education</label><input onChange={this.handleChangeeducation} type="text" class="form-control" placeholder="college" /></div>
                                 </div>
+                                <Form.Group as={Col} controlId="formGrid Experience">
+                                    <Form.Label > Experience</Form.Label>
+                                    <Form.Select onChange={this.handleChangeexperience} defaultValue="Choose...">
+                                        <option>Entry Level</option>
+                                        <option>Intermediate</option>
+                                        <option> Expert</option>
+                                    </Form.Select>
+                                </Form.Group>
+                                <Form.Group as={Col} controlId="formGrid Experience">
+                                    <Form.Label > Job Description</Form.Label>
+                                    <Form.Select onChange={this.handleChangejobdescription} defaultValue="Choose...">
+                                        <option>Architecture</option>
+                                        <option>Designer</option>
+                                        <option> Photography</option>
+                                        <option> Software Development</option>
+                                    </Form.Select>
+                                </Form.Group>
+                               
                                 <div class="row mt-3">
-                                    <div class="col-md-6"><label class="labels">Country</label><input onChange={this.handleChangecountry} type="text" class="form-control" placeholder="country"  /></div>
-                                    <div class="col-md-6"><label class="labels">State/Region</label><input onChange={this.handleChangeregion} type="text" class="form-control"  placeholder="state" /></div>
+
+                                <div class="col-md-12"><label class="labels">Languages</label><input onChange={this.handleChangelauguages} type="text" class="form-control" placeholder="enter Languages: ex (English, Arabic, french)" required /></div>
                                 </div>
                                 <br />
                                 <hr />
@@ -184,18 +213,17 @@ class Accountform extends Component {
                         <div class="col-md-4">
                             <div class="p-3 py-5">
                                 <br />
-                                    <Form.Group as={Col} controlId="formGrid Experience">
-                                        <Form.Label > Experience</Form.Label>
-                                        <Form.Select onChange={this.handleChangeexperience}  defaultValue="Choose...">
-                                            <option>Entry Level</option>
-                                            <option>Intermediate</option>
-                                            <option> Expert</option>
-                                        </Form.Select>
-                                    </Form.Group>
-                                    <div class="col-md-12"><label class="labels">Job Description</label><input onChange={this.handleChangejobdescription} type="text" class="form-control" placeholder="enter Job Description"  /></div>
-                                    <div class="col-md-12"><label class="labels">Languages</label><input onChange={this.handleChangelanguages} type="text" class="form-control" placeholder="enter Languages: ex (English, Arabic, french)"  required/></div>
-                                    <div class="col-md-12"><label class="labels">Price Per/Hour</label><input onChange={this.handleChangeprice} type="text" class="form-control" placeholder="enter  Price Per/Hour"  /></div>
-                                    <div class="col-md-12"><label class="labels">Skills</label><input onChange={this.handleChangeskills} type="text" class="form-control" placeholder="enter Skills: ex (creative, teamleader, Fast Learner)"  required/></div>
+
+                                <div class="col-md-12"><label class="labels">Location</label><input onChange={this.handleChangelocation} type="text" class="form-control" placeholder="123 Main Street, That house." /></div>
+                                <div class="col-md-12"><label class="labels">Price Per/Hour</label><input onChange={this.handleChangeprice} type="text" class="form-control" placeholder="enter  Price Per/Hour" /></div>
+                                <div class="col-md-12"><label class="labels">Skills</label><input onChange={this.handleChangeskills} type="text" class="form-control" placeholder="enter Skills: ex (creative, teamleader, Fast Learner)" required /></div>
+                                <div class="col-md-12"><label class="labels">School</label><input onChange={this.handleChangeSchool} type="text" class="form-control" placeholder="University or College etc" /></div>
+                                <div className="studies">
+                                    <div class="col-md-12"><label class="labels">AreaOfStudys</label><input onChange={this.handleChangeAreaOfStudys} type="text" class="form-control" placeholder="AreaOfStudys" /></div>
+                                    <div class="col-md-12"><label class="labels">Degree</label><input onChange={this.handleChangeDegree} type="text" class="form-control" placeholder="Degree" /></div>
+                                    <div class="col-md-12"><label class="labels">From</label><input onChange={this.handleChangeFrom} type="text" class="form-control" placeholder="From" /></div>
+                                    <div class="col-md-12"><label class="labels">To</label><input onChange={this.handleChangeTo} type="text" class="form-control" placeholder="To" /></div>
+                                </div>
                             </div><br />
                             {/* <div class="col-md-12"><label class="labels">Experience in Designing</label><input type="text" class="form-control" placeholder="experience"  /></div> <br />
                         <div class="col-md-12"><label class="labels">Additional Details</label><input type="text" class="form-control" placeholder="additional details"  /></div> */}
