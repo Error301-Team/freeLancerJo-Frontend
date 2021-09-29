070a758cbd1f431c0242dbafcff9c0a80939417e
WasemIdres <thewasem7915@gmail.com>
2021-09-29 11:46:31 +0300

    css

--- src/components/FreeLancers.js
+++ src/components/FreeLancers.js
@@ -9,6 +9,7 @@ import {
     FloatingLabel,
     Button
 } from 'react-bootstrap';
+import Phtography1 from './Phtography1';
 class Phtography extends Component {
     constructor(props) {
         super(props);
@@ -17,6 +18,8 @@ class Phtography extends Component {
             showFilteredUsers: false,
             search: "",
             saver: "",
+            user: {},
+            showUser: false
         }
     }
     componentDidMount = async () => {
@@ -70,44 +73,71 @@ class Phtography extends Component {
 
         }
     }
-
+    show = (Element) => {
+        this.setState({
+            showUser: true,
+            user: Element,
+        })
+    }
+    hide = () => {
+        this.setState({
+            showUser: false,
+        })
+    }
     render() {
+        if (this.state.showUser) {
+            return <>
+                <Spinner />
+                <Button variant="warning" onClick={this.hide} >Back</Button>
+                <Phtography1 job_describtion={this.state.user.job_describtion}
+                    name={this.state.user.name}
+                    phoneNumber={this.state.user.phoneNumber}
+                    email={this.state.user.email}
+                    location={this.state.user.location}
+                    lauguages={this.state.user.lauguages}
+                    skills={this.state.user.skills}
+                    experience={this.state.user.experience}
+                    price={this.state.user.price}
+                    img={this.state.user.img}
+                />
+            </>
+        }
         return (
             <>
                 <Spinner />
                 <br />
                 <div className="gridCards1">
-                <h1 class="h1FreeLancer">Our FreeLancers</h1>
-                <hr />
-                <h5>Filter by the field of work</h5>
-                <br />
-                <form class="formOption" onSubmit={this.handleSubmit} >
-                    <Col md className="DropDownMenu">
-                        <FloatingLabel controlId="floatingSelect" label="">
-                            <Form.Select onChange={(e) => { this.handleSelect(e) }} className="bg-white  py-3" >
-                                <option value={"all"}>Category :</option>
-                                <option value={"Architecture"}>Architecture </option>
-                                <option value={"Designer"}>Designer </option>
-                                <option value={"Photography"}>Photography </option>
-                                <option value={"Software Development"}>Software Development </option>
-                            </Form.Select>
-                        </FloatingLabel>
-                    </Col>
-                    <div class="buttonOption">
-                        <Button style={{marginBottom: "15px"}} variant="warning" as="input" type="submit" value="Click Me To Filter" />{' '}
-                    </div>
-                </form>
+                    <h1 class="h1FreeLancer">Our FreeLancers</h1>
+                    <hr />
+                    <h5>Filter by the field of work</h5>
+                    <br />
+                    <form class="formOption" onSubmit={this.handleSubmit} >
+                        <Col md className="DropDownMenu">
+                            <FloatingLabel controlId="floatingSelect" label="">
+                                <Form.Select onChange={(e) => { this.handleSelect(e) }} className="bg-white  py-3" >
+                                    <option value={"all"}>Category :</option>
+                                    <option value={"Architecture"}>Architecture </option>
+                                    <option value={"Designer"}>Designer </option>
+                                    <option value={"Photography"}>Photography </option>
+                                    <option value={"Software Development"}>Software Development </option>
+                                </Form.Select>
+                            </FloatingLabel>
+                        </Col>
+                        <div class="buttonOption">
+                            <Button style={{ marginBottom: "15px" }} variant="warning" as="input" type="submit" value="Click Me To Filter" />{' '}
+                        </div>
+                    </form>
                 </div>
                 <div className="gridCards">
                     {console.log(this.filterArray(this.state.users))}
                     {this.filterArray(this.state.users).map(Element => {
                         return (
-                            <div style={{ borderRadius: "15px"}}>
-                                <div class="image-flip" style={{ borderRadius: "15px"}}>
-                                    <div class="mainflip flip-0" style={{ borderRadius: "15px"}}>
-                                        <div class="frontside" style={{ borderRadius: "15px"}}>
-                                            <div class="card" style={{ borderRadius: "15px"}}>
-                                                <div class="card-body text-center" style={{ backgroundColor: 'white', borderRadius: "15px"}}>
+                            <div onClick={e => this.show(Element)} style={{ borderRadius: "15px" }}>
+                                <div class="image-flip" style={{ borderRadius: "15px" }}>
+                                    <div class="mainflip flip-0" style={{ borderRadius: "15px" }}>
+                                        <div class="frontside" style={{ borderRadius: "15px" }}>
+                                            <div class="card" style={{ borderRadius: "15px" }}>
+                                                <div class="card-body text-center" style={{ backgroundColor: 'white', borderRadius: "15px" }}>
                                                     <p><img class=" img-fluid" style={{ objectFit: 'cover' }} src={Element.img} alt="card image" /></p>
                                                     <h5 class="card-title">{Element.name}</h5>
                                                     <p class="card-text">{Element.job_describtion}</p>
@@ -117,9 +147,9 @@ class Phtography extends Component {
                                                 </div>
                                             </div>
                                         </div>
-                                        <div class="backside" style={{ borderRadius: "15px"}}>
-                                            <div class="card" style={{ borderRadius: "15px"}}>
-                                                <div class="card-body text-center mt-4" style={{ borderRadius: "15px"}}>
+                                        <div class="backside" style={{ borderRadius: "15px" }}>
+                                            <div class="card" style={{ borderRadius: "15px" }}>
+                                                <div class="card-body text-center mt-4" style={{ borderRadius: "15px" }}>
                                                     <h6 class="card-title">Experience:</h6><p> {Element.experience}</p>
                                                     {/* <h6 class="card-title">Education : "{Element.Education}"</h6> */}
                                                     <h6 class="card-title">Languages:</h6><p> {Element.lauguages}</p>
