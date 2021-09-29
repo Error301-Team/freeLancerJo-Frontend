85a1023f342cf325037eaa7f820f6a41df39bcda
essam omar <essam.93@hotmail.com>
2021-09-29 22:52:59 +0300

    2

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ ReactDOM.render(
   <Auth0Provider
     domain={process.env.REACT_APP_AUTH0_DOMAIN}
     clientId={process.env.REACT_APP_AUTH0_CLIENTID}
-    redirectUri="https://focused-hawking-68379b.netlify.app/logintype"
+    redirectUri="http://localhost:3000/logintype"
+    
   >
   <App />
   </Auth0Provider>
