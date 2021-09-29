9b347661854b5107c04f67d4ec03431f8f01e381
essam omar <essam.93@hotmail.com>
2021-09-29 23:19:47 +0300

    6

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ ReactDOM.render(
   <Auth0Provider
     domain={process.env.REACT_APP_AUTH0_DOMAIN}
     clientId={process.env.REACT_APP_AUTH0_CLIENTID}
-    redirectUri="https://focused-hawking-68379b.netlify.app/"
+    redirectUri="https://focused-hawking-68379b.netlify.app/ourmission"
     
   >
   <App />
